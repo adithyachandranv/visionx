be5a88982dc9ed33f06c9e0fa443e88ed24b58a9
adithyachandranv <adithyachandranv@gmail.com>
2025-01-10 20:49:56 +0530

    updates

---
+++ public/js/hovertext.js
@@ -0,0 +1,10 @@
+
+const hoverBox = document.getElementById('box-side');
+
+hoverBox.addEventListener('mouseover', () => {
+    hoverBox.textContent = 'Events';
+});
+
+hoverBox.addEventListener('mouseout', () => {
+    hoverBox.textContent = 'Events';
+});
